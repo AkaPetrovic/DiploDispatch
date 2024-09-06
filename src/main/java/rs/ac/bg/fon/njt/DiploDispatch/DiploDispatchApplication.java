3c567cd8a61090ead72724eace6789a0a89f2a9b
AkaPetrovic <akapetrovic@gmail.com>
2024-09-06 14:49:24 +0200

    Initial commit - project created with initial structure and settings parameters

---
+++ src/main/java/rs/ac/bg/fon/njt/DiploDispatch/DiploDispatchApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.njt.DiploDispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiploDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiploDispatchApplication.class, args);
+	}
+
+}
